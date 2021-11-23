be8e7f4d368cf4470c7151cfe39969fbd7f8844a
ArtOfFugue66 <haifisch1919@gmail.com>
2021-11-23 21:27:15 +0200

    Update conf file for pairs
    Refactor paths, add chunk size of thread

--- conf/pairs.py
+++ conf/pairs.py
@@ -1,9 +1,8 @@
 BASE_DATA_DIR = "C:/WORK/MASTER/DISERTATIE/Datasets"
-FEATURES_DATA_DIR = f"{BASE_DATA_DIR}/features"
-OUTPUT_DIR = f"{BASE_DATA_DIR}/pairs"
-GENUINE_DATA_PATH = f"{OUTPUT_DIR}/positive"
-IMPOSTOR_DATA_PATH = f"{OUTPUT_DIR}/negative"
-CHUNK_SIZE = 10
+FEATURES_INPUT_DIR = f"{BASE_DATA_DIR}/features"
+PAIRS_OUTPUT_DIR = f"{BASE_DATA_DIR}/pairs"
+CHUNK_SIZE = 100
+THREAD_CHUNK_SIZE = 10
 MARGIN = 0.1  # The 'alpha' in the Contrastive Loss formula
 
 # Batch size: TODO: Determine the value for max accuracy
