4ebbfb4af2464522a026493cb6c80f04d3e89a3d
ArtOfFugue66 <haifisch1919@gmail.com>
2021-11-23 17:34:15 +0200

    Refactored names in pairs conf file

--- conf/pairs.py
+++ conf/pairs.py
@@ -1,9 +1,10 @@
-DATA_DIR = "C:/WORK/MASTER/DISERTATIE/Datasets/features"
-MARGIN = 0.1  # The 'alpha' in the Contrastive Loss formula
-CHUNK_SIZE = 10
 BASE_DATA_DIR = "C:/WORK/MASTER/DISERTATIE/Datasets"
+FEATURES_DATA_DIR = f"{BASE_DATA_DIR}/features"
 OUTPUT_DIR = f"{BASE_DATA_DIR}/pairs"
 GENUINE_DATA_PATH = f"{OUTPUT_DIR}/positive"
 IMPOSTOR_DATA_PATH = f"{OUTPUT_DIR}/negative"
+CHUNK_SIZE = 10
+MARGIN = 0.1  # The 'alpha' in the Contrastive Loss formula
+
 # Batch size: TODO: Determine the value for max accuracy
 BATCH_SIZE = 512
