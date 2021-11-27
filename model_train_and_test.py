14ae5c3f9636385a346d4a5ab9cb0ce60b948204
ArtOfFugue66 <haifisch1919@gmail.com>
2021-11-27 21:59:26 +0200

    Update training & testing program flow
    Started building logic scaffold, used calls for reading, pairing & batching; Next I need to train-test-split all pair batches & unravel pair tuples from all batches

--- model_train_and_test.py
+++ model_train_and_test.py
@@ -1,67 +1,51 @@
 import os
-
 import numpy as np
-from numpy import floor
-from tqdm import tqdm
-
-import conf
-import pairs.conf
-import pairs.utils
-from pairs.pairing import make_pairs, make_pair_batches
-from features.utils import list_to_chunks_by_size
 from loguru import logger
 
+import conf.features
+import conf.siamese
+import pairs.pairing
+import utils.general
+import utils.pairs
 
 
 @logger.catch()
 def main():
-    chunk_size = conf.CHUNK_SIZE
+    from tqdm import tqdm
 
-    ### Read files from processed dataset directory (genuine user data)
-    os.chdir(pairs.conf.GENUINE_DATA_PATH)
-    genuine_filename = os.listdir('.')
-    genuine_df = pairs.utils.read_file_list_from_dataset(genuine_filename)
+    chunk_size = conf.features.CHUNK_SIZE  # Get size of a dataset chunk from features conf file
+    features_filenames = os.listdir(conf.features.OUTPUT_DIR)  # List the features dataset directory
+    np.random.shuffle(features_filenames)  # Randomly shuffle the items in the
+    features_filenames_chunks = utils.general.list_to_chunks_by_size(features_filenames,
+                                                                     chunk_size)  # Split the list of filenames in chunks
 
-    ### Read files from processed dataset directory (impostor user data)
-    os.chdir(pairs.conf.IMPOSTOR_DATA_PATH)
-    impostor_filenames = os.listdir('.')
-    impostor_dfs = pairs.utils.read_file_list_from_dataset(impostor_filenames[:100])
+    all_chunk_batches = []
+    first_sequence_features = []
+    second_sequence_features = []
+    target_distances = []
 
-    ### Split genuine & impostor data into one DataFrame / individual keystroke sequence
-    genuine_sequences = pairs.utils.split_by_section_id(genuine_df)
-    impostor_sequences = pairs.utils.split_by_section_id(impostor_dfs)
+    for chunk_index, chunk in enumerate(features_filenames_chunks):  # Iterate through the chunks of filenames
+        chunk_of_features = utils.pairs.read_features_from_dataset(chunk, chunk_index)  # Read each file in DataFrames
+        chunk_genuine_pairs, chunk_impostor_pairs = pairs.pairing.make_pairs_from_features_dfs(chunk_of_features,
+                                                                                               chunk_index)  # Make genuine & impostor pairs for all users in the chunk
+        all_chunk_batches.append(pairs.pairing.make_pair_batches(chunk_genuine_pairs,
+                                                                 chunk_impostor_pairs,
+                                                                 batch_size=conf.siamese.BATCH_SIZE,
+                                                                 shuffle_batches_flag=True))  # Make batches using the genuine & impostor pairs of this chunk
+    # Get total number of batches
+    total_no_batches = len(all_chunk_batches)
+    # TODO: Perform train-test-validation split on list of batches
+    # train_batches, test_batches, validation_batches = np.split(all_chunk_batches, [int(len())])
 
-    ### Make genuine pairs
-    genuine_pairs = make_pairs(genuine_sequences, )
-    # Make impostor pairs
-    impostor_pairs = make_impostor_pairs(genuine_sequences, impostor_sequences)
+    for batch_index, batch in tqdm(enumerate(all_chunk_batches), total=len(all_chunk_batches),
+                                   desc=f"[BATCHES] Unraveling sequences from batches"):
+        for seq1, seq2, target_distance in batch:
+            first_sequence_features.append(seq1)
+            second_sequence_features.append(seq2)
+            target_distances.append(target_distance)
 
-    ### NOTE: "The pairs were chosen randomly in each training batch...
-    ### Shuffle lists of genuine & impostor pairs
-    np.random.shuffle(genuine_pairs)
-    np.random.shuffle(impostor_pairs)
-
-    ### Make batches
-    no_total_batches = int(floor(len(genuine_pairs) / pairs.conf.GENUINE_PAIRS_PER_BATCH))  # Each batch should have at least 1 genuine pair
-    no_total_pairs = len(genuine_pairs) + len(impostor_pairs)
-    batch_size = int(floor(no_total_pairs // no_total_batches))
-
-    # Split list of all impostor pairs into a fixed number of sub-lists
-    impostor_batches = list_to_chunks_by_size(impostor_pairs, batch_size)
-    # genuine_pairs ...
-
-    batches = []
-    for i in range(0, no_total_batches):
-        # Make batch containing the genuine pair and all corresponding impostor pairs
-        batch = impostor_batches[i]
-        batch.append(genuine_pairs[i])
-        batches.append(batch)
-
-
-
-
-    # TODO: Perform train_test_validation split on the list of batches
-    pass
+        # TODO: Perform train_test_validation split on the list of batches
+        all_batches_data = (first_sequence_features, second_sequence_features, target_distances)
 
 
 if __name__ == "__main__":
