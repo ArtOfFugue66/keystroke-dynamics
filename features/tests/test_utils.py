1aafd043de612dc8bd7924474b67d6a439dd0c00
ArtOfFugue66 <haifisch1919@gmail.com>
2021-11-30 01:28:19 +0200

    Remove test case for removed utility fcn

--- features/tests/test_utils.py
+++ features/tests/test_utils.py
@@ -21,24 +21,3 @@ class TestUtilityFunctions(unittest.TestCase):
 
         for i in range(0, len(expected)):
             self.assertListEqual(actual_items[i], expected[i])
-
-    def test_list_to_chunks_by_count_fcn(self):
-        from utils.general import list_to_chunks_by_count
-
-        # Arrange
-        test_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-        test_no_chunks = 4
-        expected = [[1, 2, 3], [4, 5, 6],  [7, 8], [9, 10]]
-        # Act
-        actual = list_to_chunks_by_count(test_list, test_no_chunks)
-        # Assert
-        # Get items from generator
-        actual_items = []
-        for actual_item in actual:
-            actual_items.append(actual_item)
-
-        self.assertEqual(len(actual_items), len(expected))
-
-        for i in range(0, len(expected)):
-            self.assertListEqual(actual_items[i], expected[i])
-
