4a9d501e7280f89d1739f07caeb10a6a2400aabc
ArtOfFugue66 <haifisch1919@gmail.com>
2021-11-30 01:21:25 +0200

    Update main code logic
    Reading fewer files to enable faster debugging; Fixed batch accumulation by using 'list.extend()'; Added train-test-validation split for batches; Added batch unraveling to obtain training/testing/validation-ready samples & target distances

--- model_train_and_test.py
+++ model_train_and_test.py
@@ -11,41 +11,39 @@ import utils.pairs
 
 @logger.catch()
 def main():
-    from tqdm import tqdm
+    from siamese.siamese import make_siamese
 
     chunk_size = conf.features.CHUNK_SIZE  # Get size of a dataset chunk from features conf file
     features_filenames = os.listdir(conf.features.OUTPUT_DIR)  # List the features dataset directory
     np.random.shuffle(features_filenames)  # Randomly shuffle the items in the
-    features_filenames_chunks = utils.general.list_to_chunks_by_size(features_filenames,
+    features_filenames_chunks = utils.general.list_to_chunks_by_size(features_filenames[:chunk_size*2],
                                                                      chunk_size)  # Split the list of filenames in chunks
 
     all_chunk_batches = []
-    first_sequence_features = []
-    second_sequence_features = []
-    target_distances = []
 
     for chunk_index, chunk in enumerate(features_filenames_chunks):  # Iterate through the chunks of filenames
         chunk_of_features = utils.pairs.read_features_from_dataset(chunk, chunk_index)  # Read each file in DataFrames
         chunk_genuine_pairs, chunk_impostor_pairs = pairs.pairing.make_pairs_from_features_dfs(chunk_of_features,
                                                                                                chunk_index)  # Make genuine & impostor pairs for all users in the chunk
-        all_chunk_batches.append(pairs.pairing.make_pair_batches(chunk_genuine_pairs,
+        all_chunk_batches.extend(pairs.pairing.make_pair_batches(chunk_genuine_pairs,
                                                                  chunk_impostor_pairs,
                                                                  batch_size=conf.siamese.BATCH_SIZE,
                                                                  shuffle_batches_flag=True))  # Make batches using the genuine & impostor pairs of this chunk
     # Get total number of batches
     total_no_batches = len(all_chunk_batches)
-    # TODO: Perform train-test-validation split on list of batches
-    # train_batches, test_batches, validation_batches = np.split(all_chunk_batches, [int(len())])
-
-    for batch_index, batch in tqdm(enumerate(all_chunk_batches), total=len(all_chunk_batches),
-                                   desc=f"[BATCHES] Unraveling sequences from batches"):
-        for seq1, seq2, target_distance in batch:
-            first_sequence_features.append(seq1)
-            second_sequence_features.append(seq2)
-            target_distances.append(target_distance)
-
-        # TODO: Perform train_test_validation split on the list of batches
-        all_batches_data = (first_sequence_features, second_sequence_features, target_distances)
+    # Perform train-test-validation split on list of batches
+    train_batches, test_batches, validation_batches = all_chunk_batches[:int(total_no_batches*0.6)], \
+                                                      all_chunk_batches[int(total_no_batches*0.6):int(total_no_batches*0.8)], \
+                                                      all_chunk_batches[int(total_no_batches * 0.8):]
+    # Unravel keystroke pairs from all batches
+    # TODO: len(train_first_sequences) & len(validation_first_sequences) not multiples of 512,
+    #       BECAUSE the last batch in the 'all_chunk_batches' list is not 512 pairs long. Fix this in pairs.pairing.make_pair_batches()
+    train_first_sequences, train_second_sequences, train_target_distances = utils.general.unravel_batches(train_batches)
+    test_first_sequences, test_second_sequences, test_target_distances = utils.general.unravel_batches(test_batches)
+    validation_first_sequences, validation_second_sequences, validation_target_distances = utils.general.unravel_batches(validation_batches)
+    # Get a siamese RNN model object
+    batch_size, input_shape, emb_dims = conf.siamese.BATCH_SIZE, conf.siamese.INPUT_SHAPE, conf.siamese.EMBEDDING_DIMENSIONS
+    siamese_model = make_siamese(batch_size, input_shape, emb_dims)
 
 
 if __name__ == "__main__":
