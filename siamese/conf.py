de0dec8465db9b6e9c0bcb7ed52cce5c36eea06b
ArtOfFugue66 <haifisch1919@gmail.com>
2021-12-09 01:11:40 +0200

    Update 'siamese' conf file
    Changed TIMESTEPS parameter value to be equal to the value used as SEQUENCE_LENGTH when processing the features of the dataset

--- siamese/conf.py
+++ siamese/conf.py
@@ -1,7 +1,9 @@
+import features.conf
+
 # Batch size: number of samples in a training/test/validation batch
-# Timesteps: number of keystrokes in a sequence
+# Timesteps: number of keystrokes in a sequence (NOTE: Should be equal to the sequence length used when computing the dataset features)
 # Number of features: 4 (HOLD_LATENCY, INTERKEY_LATENCY, PRESS_LATENCY, RELEASE_LATENCY)
-BATCH_SIZE, TIMESTEPS, NO_FEATURES = 512, 70, 4
+BATCH_SIZE, TIMESTEPS, NO_FEATURES = 512, features.conf.SEQUENCE_LENGTH, 4
 INPUT_SHAPE = (TIMESTEPS, NO_FEATURES)
 EMBEDDING_DIMENSIONS = 128
 
