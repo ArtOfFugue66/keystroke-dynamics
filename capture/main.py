651fd442ffbd763cfcbd6386290e26e70920fe8a
ArtOfFugue66 <haifisch1919@gmail.com>
2022-01-21 22:15:28 +0200

    Update after playing around with git too much

--- capture/main.py
+++ capture/main.py
@@ -1,59 +1,55 @@
 from __future__ import annotations
+
+from typing import List, Tuple
+
 import keyboard as kb
 import numpy as np
 import pandas as pd
 
 import conf
 
-finished_capturing = False
 event_list = []
+sequence_list = []
 no_up_events = 0
 
+conf_data = conf.get_conf_data()
+max_seq_len = conf_data['MAX_SEQUENCE_LENGTH']  # Used to eliminate the need of searching 'conf_data' dict in callback
 
-def event_list_to_df() -> pd.DataFrame | None:
+def event_list_to_df(event_pairs: List[Tuple[float, float, str]], section_id: int) -> pd.DataFrame | None:
     """
-    TODO: Pair up keyup and keydown events
+    TODO: Redo this function last, entirely
     :return:
     """
-    global event_list
+    import conf
 
-    press_times, release_times = [], []
+    current_conf_data = conf.get_conf_data()
+    press_times, release_times, letters = [], [], []
 
-    for i, event1 in enumerate(event_list):
-        if event1.event_type == "down":
-            press_times.append(event1.time)
-            for event2 in event_list[i:]:  # Search for a later event
-                if event2.scan_code == event1.scan_code and event2.event_type == "up":  # with the same scan code
-                    release_times.append(event2.time)
-                    event_list.remove(event1)  # Remove the two events from the list
-                    event_list.remove(event2)
-
-    try:
-        # Raise an AssertionError if the lengths of the two lists are not equal
-        assert len(press_times) == len(release_times)
-    except AssertionError:
-        print("[ERROR] Nr of keydown timestamps & keyup timestamps does not match!")
-        return None
+    for pair in event_pairs:
+        press_times.append(pair[0])  # KEYDOWN timestamp
+        release_times.append(pair[1])  # KEYUP timestamp
+        letters.append(pair[2])  # Name of key that was typed
 
     df = pd.DataFrame()
 
-    participant_id_col = pd.Series(conf.PARTICIPANT_ID * len(press_times), dtype=np.int32)
+    participant_id_col = pd.Series(current_conf_data['PARTICIPANT_ID'] * len(press_times), dtype=np.int32)
     df['PARTICIPANT_ID'] = participant_id_col
-    test_section_id_col = pd.Series(conf.TEST_SECTION_ID * len(press_times), dtype=np.int32)
+    test_section_id_col = pd.Series(section_id * len(press_times), dtype=np.int32)
     df['TEST_SECTION_ID'] = test_section_id_col
     press_times_col = pd.Series(press_times, dtype=np.float64)
     df['PRESS_TIMES'] = press_times_col
     release_times_col = pd.Series(release_times, dtype=np.float64)
     df['RELEASE_TIMES'] = release_times_col
+    letters_col = pd.Series(letters, dtype=str)
+    df['LETTER'] = letters_col
 
     return df
 
+
 def write_keystroke_data():
     """
-    TODO: Later on, implement functions that make DataFrames from JSON data & call them here
     :return: None
     """
-    global event_list
 
     with open(conf.FILENAME, "w+") as file_handle:
         df = event_list_to_df()
@@ -77,15 +73,14 @@ def handle_key_hold(kb_event: kb.KeyboardEvent) -> bool:
 
 
 def keypress_callback(kb_event: kb.KeyboardEvent) -> None:
-    global finished_capturing, event_list, no_up_events
-
-    if no_up_events == conf.SEQUENCE_LENGTH:  # If enough keyup events are captured
-        finished_capturing = True  # Set key capture stop flag
-        write_keystroke_data()  # Write user's session data to the disk
-        return
+    global event_list, no_up_events, max_seq_len
 
     if len(event_list) == 0:  # If this is the first event
         event_list.append(kb_event)  # Skip checks and just add the event to the list
+        return
+
+    if no_up_events == max_seq_len:  # If enough keyup events are captured
+        print("[INFO] Sequence length achieved. Press ESC.\n")
         return
 
     if kb_event.event_type == "down":
@@ -98,13 +93,59 @@ def keypress_callback(kb_event: kb.KeyboardEvent) -> None:
         no_up_events += 1  # Keep count of keyup events that occur
 
 
+def process_sequence_events():
+    global sequence_list
+
+    sequence_timestamp_pairs = []
+    sequence_timestamp_pairs_list = [[]]
+
+    for sequence in sequence_list:
+        # Pair events together
+        keydown_events = [ev for ev in sequence if ev.event_type == "down"]
+        keyup_events = [ev for ev in sequence if ev.event_type == "up"]
+
+        for up_ev in keyup_events:  # Look through KEYUP events
+            for down_ev in keydown_events:
+                if down_ev.scan_code == up_ev.scan_code:  # Found the corresponding KEYDOWN event
+                    sequence_timestamp_pairs.append((down_ev.time, up_ev.time, up_ev.name))  # Save the timestamps of both events
+                    keydown_events.remove(down_ev)  # Remove KEYDOWN event from list in order to not use it again
+                    break
+
+        sequence_timestamp_pairs = sorted(sequence_timestamp_pairs, key=lambda x: x[0], reverse=False)  # Sort sequence pairs by KEYDOWN timestamps to ensure they are ordered correctly
+        sequence_timestamp_pairs_list.append(sequence_timestamp_pairs)  # Save list of pair timestamps for current sequence
+
+    return sequence_timestamp_pairs_list
+
+
 def main():
-    global finished_capturing
-    kb.hook(keypress_callback)
-    kb.wait()
-    if finished_capturing:
-        kb.unhook_all()
+    from keyboard import hook, wait, unhook_all
+
+    global event_list
+    global sequence_list
+    global no_up_events
+    # TODO: Test this shit cuz it's late and I ain't feeling like it right now
+    conf.update_participant_id()  # Increment participant ID in conf file
+
+    print("[INFO] Capturing keystroke data...")
+    for i in range(conf_data['NUM_SECTIONS_TO_CAPTURE']):
+        print(f"[INFO] Capturing data for sequence #{i}")
+        hook(keypress_callback)
+        wait('esc')
+        unhook_all()
+
+        no_up_events = 0  # Reset counter for KEYUP events to suppress message in callback on 2nd+ iterations
+        sequence_list.append(event_list)  # Save list of events for later processing
+        event_list = []  # Empty the list of events, to be populated in the next sequence
+
+    all_sequences_pairs = process_sequence_events()
+    dfs = []
+    for pair_index, pairs in enumerate(all_sequences_pairs):
+        dfs.append(event_list_to_df(pairs, pair_index + 1))
+
+    final_df = pd.concat(dfs, axis='rows')
+    print("DONE")
 
 
 if __name__ == "__main__":
     main()
+
---
+++ train-and-test.py
@@ -0,0 +1,67 @@
+import os
+import numpy as np
+from loguru import logger
+
+from features import conf as features_conf
+from siamese import conf as siamese_conf
+from common import utils as common_utils
+from pairs import utils as pairing_utils
+
+
+@logger.catch()
+def main():
+    from siamese.utils import make_siamese
+
+    chunk_size = features_conf.CHUNK_SIZE  # Get size of a dataset chunk from features conf file
+
+    # List the features dataset directory
+    features_filenames = os.listdir(features_conf.OUTPUT_DIR)
+
+    # Randomly shuffle the dataset filenames list
+    np.random.shuffle(features_filenames)
+
+    # Split the list of filenames in chunks
+    features_filenames_chunks = common_utils.list_to_chunks_by_size(features_filenames[:chunk_size * 2], chunk_size)
+
+    all_chunk_batches = []  # List to hold batches from all chunks
+
+    # Iterate through the chunks of filenames
+    for chunk_index, chunk in enumerate(features_filenames_chunks):
+        # Read each file in DataFrames
+        chunk_of_features = pairing_utils.read_features_from_dataset(chunk, chunk_index)
+        # Make genuine & impostor pairs for all users in the chunk
+        chunk_genuine_pairs, chunk_impostor_pairs = pairing_utils.make_pairs_from_features_dfs(dict(zip(chunk, chunk_of_features)), chunk_index)
+        # Make batches using the genuine & impostor pairs of this chunk
+        all_chunk_batches.extend(common_utils.make_pair_batches(chunk_genuine_pairs,
+                                                                chunk_impostor_pairs,
+                                                                batch_size=siamese_conf.BATCH_SIZE,
+                                                                shuffle_batches_flag=True))
+
+    # Perform train-test-validation split on list of batches
+    train_batches, test_batches, validation_batches = common_utils.test_train_val_split(all_chunk_batches, (0.8, 0.2, 0.2))
+
+    # Unravel keystroke pairs from all batches, for training, testing & validation
+    train_first_sequences, train_second_sequences, train_target_distances = common_utils.unravel_batches(train_batches)
+    test_first_sequences, test_second_sequences, test_target_distances = common_utils.unravel_batches(test_batches)
+    validation_first_sequences, validation_second_sequences, validation_target_distances = common_utils.unravel_batches(validation_batches)
+
+    # Get a siamese RNN model object
+    batch_size, input_shape, emb_dims = siamese_conf.BATCH_SIZE, siamese_conf.INPUT_SHAPE, siamese_conf.EMBEDDING_DIMENSIONS
+    siamese_model = make_siamese(batch_size, input_shape, emb_dims)
+
+    # Convert keystroke & target distances sequences from tf.EagerTensor to np.ndarray
+    train_first_sequences = np.asarray(train_first_sequences)
+    train_second_sequences = np.asarray(train_second_sequences)
+    train_target_distances = np.asarray(train_target_distances)
+
+    history = siamese_model.fit(
+        x=(train_first_sequences, train_second_sequences),
+        y=train_target_distances,
+        batch_size=siamese_conf.BATCH_SIZE,
+        epochs=10,
+        shuffle=False
+    )
+
+
+if __name__ == "__main__":
+    main()
